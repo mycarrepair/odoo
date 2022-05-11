232a291028e82344ae1e5e43d21b629a5e71b105
Natsagaa <natsagaa_sw>
2022-05-11 16:48:20 +0800

    MOD: taxtinbutton add template

--- l10n_mn_pos_tax_integration/static/src/js/tax_tin_button.js
+++ l10n_mn_pos_tax_integration/static/src/js/tax_tin_button.js
@@ -2,26 +2,21 @@ odoo.define('l10n_mn_pos_tax_integration.taxtinbutton', function (require) {
 "use strict";
 
     const { Gui } = require('point_of_sale.Gui');
-    const PosComponent  = require('point_of_sale.PosComponent');
-    const AbstractAwaitablePopup =    require('point_of_sale.AbstractAwaitablePopup');
+    const PosComponent = require('point_of_sale.PosComponent');
+    const { posbus } = require('point_of_sale.utils');
+    const ProductScreen = require('point_of_sale.ProductScreen');
+    const { useListener } = require('web.custom_hooks');
     const Registries = require('point_of_sale.Registries');
-    const ProductItem = require('point_of_sale.ProductItem');
-    const PaymentScreen = require('point_of_sale.PaymentScreen');
-
-    const TaxTinButton = (PaymentScreen) =>
-        class extends PaymentScreen {
-            constructor() {
-                super(...arguments);
-            }
-            IsRegisterTin() {
-                console.log("Click Register TIN");
-            }
-            IsRegisterEbarimtId() {                
-                console.log("Click IsRegister Ebarimt Id");
-            }
+    class TaxTinButton extends PosComponent {
+        IsRegisterTin() {
+            console.log("Click Register TIN");
         }
-    
-    Registries.Component.extend(PaymentScreen, TaxTinButton);
-
+        IsRegisterEbarimtId() {                
+            console.log("Click IsRegister Ebarimt Id");
+        }
+    }
+    TaxTinButton.template = 'TaxTinButton';
+    Registries.Component.add(TaxTinButton);
     return TaxTinButton;
+    
 });
